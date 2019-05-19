c74ed6628eb7852161868ab824b03c9ce36070fe
SunHabin <sunho8692@naver.com>
2019-05-19 20:28:15 +0900

    test

--- booksearch_server.js
+++ booksearch_server.js
@@ -1,32 +1,31 @@
-// 네이버 검색 Open API test
-
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
+//var bodyParser = require('body-parser');
 
-var client_id = 'lD4obV87D0slswIijir6';
-var client_secret = 'anU63Lz7eq';
+app.use(express.json());
 
-app.get('/search/book', function(req, res) {
-    // JSON 결과
-    console.log(req.query.query)
-    var api_url = 'https://openapi.naver.com/v1/search/book.json?query=' + encodeURI(req.query.query);
+//support URL-encode body
+app.use(bodyParser.urlencoded({extended: true}));
 
-    var request = require('request');
-    var options = {
-        url : api_url,
-        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret':client_secret}
-    };
-    request.get(options, function (error,response,body) {
-        if(!error && response.statusCode == 200) {
-            res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-            res.end(body);
-        } else {
-            res.status(response.statusCode).end();
-            console.log('error = ' + response.statusCode);
-        }
-    });
+var con = mysql.createConnection({
+
+    host: '220.149.242.12',
+    user: 'root',
+    password: 'bean0403',
+    database: 'jajus'
 });
 
-app.listen(3000, function () {
-  console.log('http://127.0.0.1:3000/search/book?query=검색어 app listening on port 3000!');
+var server = app.listen(3000, function() {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("server start");
+
 });
+
+con.connect(function(error){
+    if(!!error)console.log("error");
+    else console.log("connected");
+});
+
