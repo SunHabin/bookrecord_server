b0b7f530374ed3846482bda0c31091b510be197c
SunHabin <sunho8692@naver.com>
2019-05-21 22:10:26 +0900

    Update booksearch_server2.js

--- booksearch_server2.js
+++ booksearch_server2.js
@@ -47,8 +47,8 @@ app.get('/search/book/:ISBN', function(req, res) {
     };
     request.get(options, function (error,response,body) {
         if(!error && response.statusCode == 200) {
-            //res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-            res.end(body);
+            res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+            res.end(body.items);
         } else {
             res.status(response.statusCode).end();
             console.log('error = ' + response.statusCode);
