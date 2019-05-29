7bc698b2ab1d82bf3515702688e7805684ab734f
SunHabin <sunho8692@naver.com>
2019-05-29 10:41:55 +0900

    Update booksearch_server2.js

--- booksearch_server2.js
+++ booksearch_server2.js
@@ -202,7 +202,7 @@ app.post('/saveBook/', function(req,res){
     var category = '';
     console.log(req.body);
     console.log(req.body.ISBN);
-
+try{
     con.query('SELECT * FROM book_all where ISBN = ?',req.body.ISBN, function(error,rows,fields){
         if(!!error) {
             console.log("error1");
@@ -216,7 +216,8 @@ app.post('/saveBook/', function(req,res){
             category = rows[0].category;
         }
     });
-
+}
+finally{
     var user_name = "'" + req.body.user_name + "'";
     var ISBN = "'" + req.body.ISBN + "'";
     read_date = "'" + read_date +"'";
@@ -236,4 +237,5 @@ app.post('/saveBook/', function(req,res){
             res.end('success insert!');
         }
     })
+}
 })
