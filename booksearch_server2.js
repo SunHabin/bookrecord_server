8ea71131fff2ed41db1a8c0aa4fb934be3d2c657
SunHabin <sunho8692@naver.com>
2019-05-30 09:38:45 +0900

    Update booksearch_server2.js

--- booksearch_server2.js
+++ booksearch_server2.js
@@ -256,7 +256,7 @@ app.get('/statCategory/:name',function(req,res){
     //TEST
     console.log("statCategory");
     
-    con.query('SELECT * FROM user_category where user_name = ?', req.params.name, function(error, rows, fields) {
+    con.query('SELECT * FROM user_category where user_name = ? order by ca_count desc limit 3', req.params.name, function(error, rows, fields) {
         if(!!error)
             console.log(error);
         else{
